d757ef5a23d847248e149332c33b78886c499794
Bruno Oliveira <bruno.oss@outlook.com>
2025-09-30 18:08:06 -0300

    4. Testando TestCase

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 
-from test_case import TestCase
+from test_case_test import TestCaseTest
 from test_result import TestResult
 
-class MyTest(TestCase):
-    def set_up(self):
-        print('set_up')
 
-    def tear_down(self):
-        print('tear_down')
+result = TestResult()
 
-    def test_a(self):
-        print('test_a')
+test = TestCaseTest('test_result_success_run')
+test.run(result)
 
-    def test_b(self):
-        print('test_b')
+test = TestCaseTest('test_result_failure_run')
+test.run(result)
 
-    def test_c(self):
-        print('test_c')
+test = TestCaseTest('test_result_error_run')
+test.run(result)
 
+test = TestCaseTest('test_result_multiple_run')
+test.run(result)
 
-if __name__ == "__main__":
+test = TestCaseTest('test_was_set_up')
+test.run(result)
 
-    result = TestResult()
+test = TestCaseTest('test_was_run')
+test.run(result)
 
-    test = MyTest('test_a')
-    test.run(result)
+test = TestCaseTest('test_was_tear_down')
+test.run(result)
 
-    test = MyTest('test_b')
-    test.run(result)
+test = TestCaseTest('test_template_method')
+test.run(result)
 
-    test = MyTest('test_c')
-    test.run(result)
-    
-    print(result.summary())
+print(result.summary())
